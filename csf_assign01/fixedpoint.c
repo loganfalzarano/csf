e18ba43f59cecacf8652fdf6384e91372b6c1a4b
TalMagdish <78189490+TalMagdish@users.noreply.github.com>
2022-01-29 18:41:23 -0500

    Updated fixpoint.c

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
+#include <stdbool.h>
 #include "fixedpoint.h"
 
 // You can remove this once all of the functions are fully implemented
@@ -36,12 +37,22 @@ Fixedpoint fixedpoint_create_from_hex(const char *hex) {
   // TODO: This isn't correct but it's an idea
 
   //Determine if there's a negative sign
-  bool is_negative false;
-  if (hex[0] == '-') is_negative = true;
+  Fixedpoint res;
+  if (hex[0] == '-') res.tag = 2;
+
+
+  //Determine if there's a negative sign
+  // bool is_negative = false;
+  // if (hex[0] == '-') is_negative = true;
+
+  // int len = strlen(hex);
+  // if (len > 34) {
+  //   hex.tag = 3;
+  // }
 
   //find whether/where the '.' occurs to split into whole and fractional
   int dot_index = -1;
-  for (; i < strlen(hex); i++) {
+  for (; dot_index < strlen(hex); dot_index++) {
     if (hex[dot_index] == '.') {
       break;
     }
@@ -55,39 +66,39 @@ Fixedpoint fixedpoint_create_from_hex(const char *hex) {
   //Otherwise there is a dot and split the hex string into two strings 
   char whole_hex[dot_index];
   char frac_hex[strlen(hex) - dot_index];
-  for (int j = 0; j < dot_index; i++) {
-    whole_frac[j] = hex[j]
+  for (int j = 0; j < dot_index; j++) {
+    whole_hex[j] = hex[j];
   }
-  for (int k = 0; k + dot_index < strlen(hex); k++)) {
-    frac_hex[k] = hex[dot_index + k]
+  for (int k = 0; k + dot_index < strlen(hex); k++) {
+    frac_hex[k] = hex[dot_index + k];
   }
 
   uint64_t whole_val;
   uint64_t frac_val;
 
-  sscanf(whole_hex, "%x", &whole_val)
-  sscanf(frac_hex, "%x", &frac_val)
+  sscanf(whole_hex, "%x", &whole_hex);
+  sscanf(frac_hex, "%x", &frac_val);
 
-  //here we want to use sscanf and bitsifting to make the conversion
+  //here we want to use sscanf and bitshifting to make the conversion
   //int store_value;
   //sscanf(hex, "%x" , &store_value);
 
-  Fixedpoint res;
-  res.whole_part = whole_val;
+  //Fixedpoint res;
+  res.whole_part = whole_hex;
   res.frac_part = frac_val;
-  res.tag = 1;
+  //res.tag = 1;
 
   assert(0);
   return res;
 }
 
 uint64_t fixedpoint_whole_part(Fixedpoint val) {
-  //TODO: Do we need to do error handling here?
+  //TODO: Do we need to do error handling here? -- I don't think so-Tal
   return val.whole_part;
 }
 
 uint64_t fixedpoint_frac_part(Fixedpoint val) {
-  //TODO: Do we need to do error handling here?
+  //TODO: Do we need to do error handling here? -- I don't think so-Tal
   return val.frac_part;
 }
 
@@ -129,7 +140,7 @@ int fixedpoint_compare(Fixedpoint left, Fixedpoint right) {
 
 int fixedpoint_is_zero(Fixedpoint val) {
   //Logan - "Don't know if this is right"
-  return val.whole_part == 0 && val.frac_part == 0;
+  return fixedpoint_is_valid(val) && val.whole_part == 0 && val.frac_part == 0;
 }
 
 int fixedpoint_is_err(Fixedpoint val) {
@@ -169,9 +180,7 @@ int fixedpoint_is_underflow_pos(Fixedpoint val) {
 }
 
 int fixedpoint_is_valid(Fixedpoint val) {
-  // TODO: implement
-  assert(0);
-  return 0;
+  return val.tag == 1 || val.tag == 2;
 }
 
 char *fixedpoint_format_as_hex(Fixedpoint val) {
